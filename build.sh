30317a7a3d6f7bc4a058a50fa65ff22ad31bfe04
I518342 <quoc.trung.hoang@sap.com>
2019-10-09 05:42:55 -0700

    Build script for release generation

--- build.sh
+++ build.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
-PLATFORMS="darwin/amd64" # amd64 only as of go1.5
-PLATFORMS="$PLATFORMS windows/amd64 windows/386" # arm compilation not available for Windows
-PLATFORMS="$PLATFORMS linux/amd64 linux/386"
-PLATFORMS="$PLATFORMS linux/ppc64 linux/ppc64le"
-PLATFORMS="$PLATFORMS openbsd/amd64" # amd64 only as of go1.6
-PLATFORMS="$PLATFORMS solaris/amd64" # as of go1.3
 
-SCRIPT_NAME=`basename "$0"`
-FAILURES=""
-SOURCE_FILE=`echo $@ | sed 's/\.go//'`
-CURRENT_DIRECTORY=${PWD##*/}
-OUTPUT=${SOURCE_FILE:-$CURRENT_DIRECTORY} # if no src file given, use current dir name
+build() {
+  PLATFORMS="darwin/amd64" # amd64 only as of go1.5
+  PLATFORMS="$PLATFORMS linux/amd64 linux/386"
 
-type setopt >/dev/null 2>&1
+  SCRIPT_NAME=`basename "$0"`
+  FAILURES=""
+  SOURCE_FILE=`echo $@ | sed 's/\.go//'`
+  CURRENT_DIRECTORY=${PWD##*/}
+  OUTPUT=${SOURCE_FILE:-$CURRENT_DIRECTORY} # if no src file given, use current dir name
 
-for PLATFORM in $PLATFORMS; do
-  GOOS=${PLATFORM%/*}
-  GOARCH=${PLATFORM#*/}
-  BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}"
-  if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
-  CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_FILENAME} $@"
-  echo "${CMD}"
-  eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
+  type setopt >/dev/null 2>&1
 
-  mv $BIN_FILENAME $(pwd)/bin/$BIN_FILENAME
-done
+  for PLATFORM in $PLATFORMS; do
+    GOOS=${PLATFORM%/*}
+    GOARCH=${PLATFORM#*/}
+    BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}"
+    if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
+    CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_FILENAME} $@"
+    echo "${CMD}"
+    eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
 
-# eval errors
-if [[ "${FAILURES}" != "" ]]; then
-  echo ""
-  echo "${SCRIPT_NAME} failed on: ${FAILURES}"
-  exit 1
-fi
+    mv $BIN_FILENAME $(pwd)/bin/$BIN_FILENAME
+  done
+
+  # eval errors
+  if [[ "${FAILURES}" != "" ]]; then
+    echo ""
+    echo "${SCRIPT_NAME} failed on: ${FAILURES}"
+    exit 1
+  fi
+}
+
+push() {
+  releases=""
+  for release in $(find bin/*); do
+    releases="$releases -a $release "
+  done
+
+  hub release create $releases "$1"
+}
+
+
+build
+push "$1"
